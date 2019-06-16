69fab30e37c5e51c4a1672fc792b083e182946e5
alejandro <alejandrojuarez675@gmail.com>
2019-06-15 22:39:24 -0300

    creo primer component

--- moneyManagerApp/src/app/app-routing.module.ts
+++ moneyManagerApp/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
-//  { path: '', loadChildren: undefined }
+  { path: '', component: MainComponent }
 ];
 @NgModule({
   imports: [
